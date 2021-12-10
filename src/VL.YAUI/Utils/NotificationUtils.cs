8e545e68b50aa7a71d062ea51098e83814d7459c
tebjan <tebjan@vvvv.org>
2021-12-10 18:04:36 +0100

    Fixes keyboard input from Stride

--- src/VL.YAUI/Utils/NotificationUtils.cs
+++ src/VL.YAUI/Utils/NotificationUtils.cs
@@ -1,5 +1,8 @@
 ﻿using Stride.Core.Mathematics;
 using System;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
 using VL.Lib.IO.Notifications;
 using VL.YAUI.Utils;
 
@@ -7,6 +10,50 @@ namespace VL.YAUI
 {
     public static class NotificationUtils
     {
+        public static string KeyCodeToUnicode(Keys key)
+        {
+            byte[] keyboardState = new byte[255];
+            bool keyboardStateStatus = GetKeyboardState(keyboardState);
+
+            if (!keyboardStateStatus)
+            {
+                return "";
+            }
+
+            uint virtualKeyCode = (uint)key;
+            uint scanCode = MapVirtualKey(virtualKeyCode, 0);
+            IntPtr inputLocaleIdentifier = GetKeyboardLayout(0);
+
+            StringBuilder result = new StringBuilder();
+            ToUnicodeEx(virtualKeyCode, scanCode, keyboardState, result, (int)5, (uint)0, inputLocaleIdentifier);
+
+            return result.ToString();
+        }
+
+        [DllImport("user32.dll")]
+        static extern bool GetKeyboardState(byte[] lpKeyState);
+
+        [DllImport("user32.dll")]
+        static extern uint MapVirtualKey(uint uCode, uint uMapType);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetKeyboardLayout(uint idThread);
+
+        [DllImport("user32.dll")]
+        static extern int ToUnicodeEx(uint wVirtKey, uint wScanCode, byte[] lpKeyState, [Out, MarshalAs(UnmanagedType.LPWStr)] StringBuilder pwszBuff, int cchBuff, uint wFlags, IntPtr dwhkl);
+
+        public static bool ToKeyPressNotification(KeyUpNotification keyUpNotification, out KeyPressNotification keyPressNotification)
+        {
+            var s = KeyCodeToUnicode(keyUpNotification.KeyData);
+            var success = s.Length == 1;
+
+            var c = success ? s[0] : ' ';
+
+            keyPressNotification = new KeyPressNotification(c, keyUpNotification.Sender);
+
+            return success;
+        }
+
         public static TResult MouseKeyboardSwitch<TResult>(object eventArg, TResult defaultResult,
             Func<MouseDownNotification, TResult> onMouseDown = null,
             Func<MouseMoveNotification, TResult> onMouseMove = null,
